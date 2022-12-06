87d562e6921e3a0b80f1d9ddbb5eea304c8b61ed
liushiju <lsj_neusoft@163.com>
2022-12-06 18:07:41 +0800

    fix: day02_作业02: 计算a+b的和

---
+++ 01.初级阶段/day02/初级阶段作业02/01.计算a加b的和/main.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS //放到第一行，一定要放到头文件前面
+#include <stdio.h>
+
+//问题路径: http://oj.lgwenda.com/problem/2
+int main()
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d\n", a + b);
+	return 0;
+}
