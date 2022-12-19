61ae42e6d428556bb8c25c986d83abc1ead6c181
liushiju <lsj_neusoft@163.com>
2022-12-19 15:39:28 +0800

    feat: scanf读取标准输入缓冲区

---
+++ 04.课上演示代码/01.初级阶段/数据类型与运算符/7.scanf读取标准输入缓冲区/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//原理对于初试不重要，机试很重要
+//缓冲区为空的时候，scanf才会阻塞
+int main()
+{
+	int i;
+	char c;
+	float f;
+	scanf("%d", &i);
+	printf("i=%d\n", i);
+	//scanf("%c", &c);
+	//printf("c=%c\n", c);
+	scanf("%f", &f);
+	printf("f=%f\n", f);
+	return 0;
+}
